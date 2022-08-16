2892d37cfa4ce2e4f64f18d049dc5c34eec51093
Agbamajnr <ulimhunyieagbama@gmail.com>
2022-08-16 03:30:36 +0100

    change to put

--- api/v1/routes/auth.route.js
+++ api/v1/routes/auth.route.js
@@ -12,7 +12,7 @@ router.get('/user', verify_token, getUser)
 router.post('/register', CreateUser)
 router.post('/login', LoginUser)
 router.post('/user/verify', verifyUser)
-router.post('/user/verifyOTP', verifyOTPsent)
+router.put('/user/verifyOTP', verifyOTPsent)
 
 router.put('/user/update', verify_token, updateUserFields)
 router.patch('/user/update/password', verify_token, changeUserPassword)
